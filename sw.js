3dee6ad5619394b09d5373c26fdb4ea2fbb41d11
cnhwndv586-cyber <cnhwndv586@privaterelay.appleid.com>
2026-08-29 03:50:19 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='memoria-v3-1';const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(e.request.method==='GET'){let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return res}).catch(()=>r)))});
